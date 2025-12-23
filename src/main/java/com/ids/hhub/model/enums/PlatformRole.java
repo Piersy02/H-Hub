7001aff519e4aa53cb361ac9ed5502eb55e426e6
Mattia Penna <mattia.penna@studenti.unicam.it>
2025-12-23 17:11:56 +0100

    Creati Enums per gestione ruoli di piattaforma e di Hackathon

---
+++ src/main/java/com/ids/hhub/model/enums/PlatformRole.java
@@ -0,0 +1,6 @@
+package com.ids.hhub.model.enums;
+
+public enum PlatformRole {
+    USER,  // Utente Registrato
+    ADMIN  // Amministratore di sistema
+}
---
+++ src/main/java/com/ids/hhub/model/enums/StaffRole.java
@@ -0,0 +1,7 @@
+package com.ids.hhub.model.enums;
+
+public enum StaffRole {
+    ORGANIZER,
+    MENTOR,
+    JUDGE
+}
