347b0c8961df1633760d5762bd46b623e4396052
matteo bolognini <matteo.bolognini@studenti.unicam.it>
2025-12-27 16:54:04 +0100

    Aggiunta di alcuni models

---
+++ src/main/java/com/ids/hhub/model/enums/HackathonState.java
@@ -0,0 +1,8 @@
+package com.ids.hhub.model.enums;
+
+public enum HackathonState {
+    REGISTRATION_OPEN, // In iscrizione
+    ONGOING,           // In corso
+    EVALUATION,        // In valutazione
+    FINISHED           // Concluso
+}
