bab9401fc9634cc95cbe312bce365d74876de4b1
matteo bolognini <matteo.bolognini@studenti.unicam.it>
2026-01-04 18:14:59 +0100

    Configurazione Autenticazione Login/Sign-in/Logout

---
+++ src/main/java/com/ids/hhub/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.ids.hhub.controller;
+
+import com.ids.hhub.dto.LoginRequestDto;
+import com.ids.hhub.dto.RegisterRequestDto;
+import com.ids.hhub.model.User;
+import com.ids.hhub.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@Tag(name = "Authentication", description = "API per la gestione della registrazione e del login utenti")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody @Valid RegisterRequestDto request) {
+        //passi il DTO al service
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequestDto dto) {
+        User user = authService.login(dto);
+        // In un sistema reale qui restituiresti un Token (JWT).
+        return ResponseEntity.ok("Login effettuato con successo! ID Utente: " + user.getId());//Implementare id user
+    }
+}
