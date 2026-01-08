0cefd523d8a3daa6e08b7bd76a65b02029c79b85
SAVERIO MARIA PIERSIGILLI <saveriom.piersigilli@studenti.unicam.it>
2026-01-08 18:31:48 +0100

    Aggiunta Ruolo Organizzatore di sistema

--- src/main/java/com/ids/hhub/model/enums/PlatformRole.java
+++ src/main/java/com/ids/hhub/model/enums/PlatformRole.java
@@ -2,5 +2,6 @@ package com.ids.hhub.model.enums;
 
 public enum PlatformRole {
     USER,  // Utente Registrato
+    EVENT_CREATOR,  // Può creare Hackathon (Organizzatore di piattaforma)
     ADMIN  // Amministratore di sistema
 }
