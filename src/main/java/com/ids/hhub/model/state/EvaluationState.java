e26435c4b0823429f5ba99467fb57c09639d34af
Mattia Penna <mattia.penna@studenti.unicam.it>
2026-01-12 19:21:04 +0100

    Aggiunta l'implementazione del Design Pattern State

---
+++ src/main/java/com/ids/hhub/model/state/EvaluationState.java
@@ -0,0 +1,15 @@
+package com.ids.hhub.model.state;
+
+
+import com.ids.hhub.model.Hackathon;
+import com.ids.hhub.model.Team;
+
+public class EvaluationState implements HackathonState {
+
+    @Override
+    public void registerTeam(Hackathon context, Team team) {
+        // Nello stato di Valutazione, le iscrizioni sono chiuse
+        throw new IllegalStateException("ERRORE: Le iscrizioni sono chiuse. I giudici stanno valutando i progetti.");
+    }
+
+}
